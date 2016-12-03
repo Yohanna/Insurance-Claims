b922a495a901356cceafdd685e03b49ce610ae45
Matthew Sherar <matthew.d.sherar@gmail.com>
2016-12-03 15:12:14 -0500

    Commented code

--- firstModel.py
+++ firstModel.py
@@ -5,7 +5,7 @@ Matthew Sherar 10093010
 Predicting cost of insurance claim for a company based on 116 categorical attributes
 and 14 numerical attributes
 
-Neural Network used is part of Keras library
+Neural Network used is part of Keras library running on Tensorflow
 
 '''
 
@@ -28,15 +28,16 @@ from sklearn.decomposition import PCA
 import sklearn.metrics
 from sklearn.metrics import mean_absolute_error
 
-train  = pd.read_csv("trainNoCats.csv")
+train  = pd.read_csv("train.csv")
 
 
 
 '''for each categroical column in the training set, the label encoder converts the 
 categorical value to a numerical one'''
-#for i in range(116):
-#	train['cat%d'%(i+1)] = preprocessing.LabelEncoder().fit_transform(train['cat%d'%(i+1)])
+for i in range(116):
+	train['cat%d'%(i+1)] = preprocessing.LabelEncoder().fit_transform(train['cat%d'%(i+1)])
 
+#create new column to specify if the claim is over $10000
 def f(row):
      if row['loss'] > 10000:
           val = 1
@@ -44,7 +45,7 @@ def f(row):
           val = 0
      return val
 
-train['large'] = train.apply(f, axis=1)
+train['large'] = train.apply(f, axis=1) #apply the function
 
 t = train[train.large == 1]  
 t1 = train[train.large == 0].sample(5880)
@@ -52,10 +53,9 @@ t = t.append(t1)
 t = t.sample(frac = 1)  
 train = t 
 
-#this was used while testing to reduce execution time 
-#train.to_csv("trainNoCats.csv")
 
-#set of 'loss' column to  be the target column we wish to predict
+
+#set of 'large' column to  be the target column we wish to predict
 y = train['large']
 #remove the loss column from the rest of the  data
 train = train.drop('loss', axis=1)
@@ -65,14 +65,16 @@ train = train.drop('large', axis=1)
 
 #split up the data into training and test sets, with a test set size of 30% 
 x_train, x_test, y_train, y_test = train_test_split(train, y, test_size=0.3)
+
+#drop other unnecassary columns
 x_train = x_train.drop("Unnamed: 0", axis=1)
 x_train = x_train.drop("id", axis =1)
-#x_train = x_train.ix[:,116:130]
 x_train = x_train.as_matrix()
 x_test = x_test.drop("Unnamed: 0", axis=1)
 x_test = x_test.drop("id", axis =1)
-#x_test = x_test.ix[:,116:130]
 x_test = x_test.as_matrix()
+y_train = y_train.as_matrix()
+#Tested with PCA
 #pca = PCA(n_components = 20)
 #x_train = pca.fit_transform(x_train)
 #y_train = pca.fit_transform(y_train)
@@ -80,14 +82,8 @@ x_test = x_test.as_matrix()
 
 
 
-#convert the data from DataFrames to arrays to be used in the neural network
-#x_train = x_train.as_matrix()
-#x_test = x_test.as_matrix()
-#y_train = y_train.as_matrix()
-
-
 model = Sequential()
-#first layer has input dimension of 130, 1024 nodes, activation function 'relu' for rectified linear unit
+#first layer has input dimension of 130, 400 nodes, activation function 'relu' for rectified linear unit
 model.add(Dense(400, input_dim=130, init='normal'))
 model.add(Activation('relu'))
 #add more layers
@@ -100,15 +96,19 @@ model.add(Activation('relu'))
 model.add(Dense(1, init='normal', activation='sigmoid'))
 
 #use mean absolute error as error calculation
-#optimizer Nadam is like Root Mean Square propogation with momentum and small learning rate
+#optimizer adadelta is like Root Mean Square propogation with momentum and small stochastic learning rate
 model.compile(loss='binary_crossentropy', optimizer='adadelta')
 
+##train the model
 model.fit(x_train, y_train, nb_epoch=10, batch_size=32)
 
+
+#predict test set data
 pred = pd.DataFrame(model.predict(x_test))
 
 yTest = pd.DataFrame(y_test)
 
+#binary high or low claim decision
 def cat(row):
 	if row[0] > 0.5:
 		val  = 1
@@ -118,10 +118,13 @@ def cat(row):
 
 pred['cat'] = pred.apply(cat, axis=1)
 
+
+#Print binary classifier metrics
 print(sklearn.metrics.classification_report(yTest, pred['cat']))
 print(sklearn.metrics.confusion_matrix(yTest, pred['cat']))
 
 
+#NN models trained on low, high, and all claims respectively
 def lowModel():
      modelLow = Sequential()
      modelLow.add(Dense(1000, input_dim=130, init='he_normal'))
@@ -165,6 +168,7 @@ def normalModel():
      modelNorm.compile(loss='mae', optimizer='adagrad')
      return modelNorm
 
+#read in data again - this has already been encoded
 train  = pd.read_csv("trainNoCats.csv")
 
 def f(row):
@@ -179,6 +183,7 @@ y = train['loss']
 
 x_train, x_test, y_train, y_test = train_test_split(train, y, test_size=0.3)
 
+#Select all data for the normal network
 n = x_train.drop('large', axis=1)
 n = n.drop('loss', axis=1)
 n = n.drop('id', axis=1)
@@ -188,32 +193,35 @@ estimatorsNorm = []
 estimatorsNorm.append(('standardize', StandardScaler()))
 estimatorsNorm.append(('mlp', KerasRegressor(build_fn=normalModel, nb_epoch=10, batch_size=50, verbose=1)))
 pipelineNorm = Pipeline(estimatorsNorm)
+#train the model
 pipelineNorm.fit(n, y_train)
 
 
+
+#select all the small claims and a few large claims for the 'low' model
 t = x_train[x_train.large == 0]
 t1 = x_train[x_train.large == 1].sample(1000)
 t = t.append(t1)
-t = t.sample(frac=1)
+t = t.sample(frac=1) #shuffles the data
 tY = t['loss']
 t = t.drop('loss', axis=1)
 t = t.drop('id', axis=1)
 t = t.drop('Unnamed: 0', axis =1)
 t = t.drop('large', axis=1)
-
 t = t.as_matrix()
 tY = tY.as_matrix()
 estimatorsLow = []
 estimatorsLow.append(('standardize', StandardScaler()))
 estimatorsLow.append(('mlp', KerasRegressor(build_fn=lowModel, nb_epoch=5, batch_size=50, verbose=1)))
 pipelineLow = Pipeline(estimatorsLow)
+#train the low model
 pipelineLow.fit(t, tY)
 
-
+#select all the high claims and some of the low claims to train the model
 k = x_train[x_train.large == 1]
 k1 = x_train[x_train.large == 0].sample(1000)
 k=k.append(k1)
-k =k.sample(frac=1)
+k =k.sample(frac=1) # shuffles the data
 kY = k['loss']
 k = k.drop('loss', axis=1)
 k = k.drop('id', axis=1)
@@ -225,18 +233,21 @@ estimatorsHigh = []
 estimatorsHigh.append(('standardize', StandardScaler()))
 estimatorsHigh.append(('mlp', KerasRegressor(build_fn=highModel, nb_epoch=30, batch_size=50, verbose=1)))
 pipelineHigh = Pipeline(estimatorsHigh)
+#train the model
 pipelineHigh.fit(k, kY)
 
-
+#putting all three models together
 x_test = x_test.drop('loss', axis=1)
 x_test = x_test.drop('large', axis=1)
 x_test = x_test.drop('Unnamed: 0', axis=1)
 x_test = x_test.drop('id', axis=1)
 x_test = x_test.as_matrix()
-pred = pd.DataFrame(model.predict(x_test))
-lowPred = pipelineLow.predict(x_test)
-highPred = pipelineHigh.predict(x_test)
-normPred = pipelineNorm.predict(x_test)
+pred = pd.DataFrame(model.predict(x_test)) #predict binary clasification
+lowPred = pipelineLow.predict(x_test) #prediction from low model
+highPred = pipelineHigh.predict(x_test) #prediction from high model
+normPred = pipelineNorm.predict(x_test) #prediction from normal model
+
+#Put all predicitions into one DataFrame
 pred['low'] = lowPred
 pred['high'] = highPred
 pred['norm'] = normPred
@@ -244,8 +255,10 @@ pred['norm'] = normPred
 y_test = pd.DataFrame(y_test)
 y_test = y_test.set_index(pred.index)
 
+#Add actual loss value to the same DataFrame
 pred['act'] = y_test
 
+#Weighted sum of the model outputs
 def out(row):
      if row[0] > 0.95:
           return (row['high']*0.75 + row['norm']*0.25)
@@ -254,12 +267,15 @@ def out(row):
 
 pred['out'] = pred.apply(out, axis=1)
 
+##Error stats
 print("Normal Error")
 print(mean_absolute_error(pred['act'], pred['norm']))
 
 print("Weighted MultiNetwork Error")
 print(mean_absolute_error(pred['act'], pred['out']))
 
+#Figure of the error graph
+'''
 fig = plt.figure()
 
 ax = fig.add_subplot(111)
@@ -270,7 +286,7 @@ ax.scatter(pred['norm'],pred['act'])
 plt.show()
 
 
-
+'''
 
 
 
